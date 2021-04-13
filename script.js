60238a22e98551d98ae16cb29112422cfc4aa518
Martina Korandova <martina.maserkap8@gmail.com>
2021-04-14 00:26:10 +0200

    Circles and crosses

--- script.js
+++ script.js
@@ -1,14 +1,29 @@
 'use strict';
 
+let playing = 'circle';
+
+let click = 0;
+const playsElm = document.querySelector('.game__player--circle');
+//console.log(playsElm);
+
 const selectButton = (event) => {
-  event.target.classList.toggle('btn--selected');
+  event.target.classList.add('btn--' + playing);
+  if (playing === 'circle') {
+    playing = 'cross';
+    playsElm.src = 'image/cross.svg';
+    playsElm.alt = 'Hraje křížek.';
+  } else {
+    playing = 'circle';
+    playsElm.src = 'image/circle.svg';
+    playsElm.alt = 'Hraje kolečko.';
+  }
+  event.target.disabled = true;
+  click++;
+  console.log(click);
 };
 
 const btnSelected = document.querySelectorAll('button');
-/* console.log(btnSelected.length); */
+// check: console.log(btnSelected.length);
 for (let i = 0; i < btnSelected.length; i += 1) {
   btnSelected[i].addEventListener('click', selectButton);
 }
-
-
-
