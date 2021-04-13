060f4918f6769532686ab206d2ecbfceea06c954
Martina Korandova <martina.maserkap8@gmail.com>
2021-04-13 13:45:55 +0200

    Buttons selected

--- script.js
+++ script.js
@@ -1,2 +1,14 @@
 'use strict';
 
+const selectButton = (event) => {
+  event.target.classList.toggle('btn--selected');
+};
+
+const btnSelected = document.querySelectorAll('button');
+/* console.log(btnSelected.length); */
+for (let i = 0; i < btnSelected.length; i += 1) {
+  btnSelected[i].addEventListener('click', selectButton);
+}
+
+
+
